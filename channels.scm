ca8d475b545363d0b95c315eeaca473406d598f8
Eric Yager <ejyager00@protonmail.com>
2026-07-29 14:45:29 -0400

    add personal channel

--- channels.scm
+++ channels.scm
@@ -15,4 +15,13 @@
         (make-channel-introduction
          "9edb3f66fd807b096b48283debdcddccfea34bad"
          (openpgp-fingerprint
-          "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA")))))
+          "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA"))))
+      (channel
+       (name 'ejyager00)
+       (url "https://github.com/ejyager00/guix-channel")
+       (branch "master")
+       (introduction
+        (make-channel-introduction
+         "03dda62faab96a84d0fab61d46366d5b9cd8894f"
+         (openpgp-fingerprint
+          "1C96 9810 49FF 9A9D 0021  D960 A632 52C7 09EC BA81")))))
